e514fb30029d33c09dda1a7a53baf1aa5d4a28b8
Bhekani Khumalo <bhekanik@gmail.com>
2023-04-26 16:53:17 +0100

    Add vim options

--- init.lua
+++ init.lua
@@ -5,3 +5,15 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.autoindent = true
+opt.smartindent = true
+opt.incsearch = true
+opt.smartcase = true
+opt.ruler = true
+opt.cursorline = true
+opt.mouse = "a"
+opt.title = true

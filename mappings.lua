21a9af820c78a8416022a35a108ece3da856b449
Bhekani Khumalo <bhekanik@gmail.com>
2023-04-26 05:24:53 +0100

    initial commit

---
+++ mappings.lua
@@ -0,0 +1,46 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["£"] = { "#", "hash symbol", opts = { nowait = true } },
+    ["0"] = { "^", "go to line start", opts = { nowait = true } },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left"},
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right"},
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down"},
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up"},
+    ["<leader>gg"] = { "<cmd> LazyGit <CR>", "Open LazyGit", opts = { silent = true } },
+  },
+  i = {
+    ["£"] = { "#", "hash symbol", opts = { nowait = true } },
+  },
+}
+
+-- more keybinds!
+M.dap = {
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>", "Toggle breakpoint" },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    },
+  },
+}
+
+M.crates = {
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
+    },
+  },
+}
+
+return M

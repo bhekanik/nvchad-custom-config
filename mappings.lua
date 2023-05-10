7429741f70a643f7809b152940dce5d24fb60b82
Bhekani Khumalo <bhekanik@gmail.com>
2023-05-10 17:49:07 +0100

    Center search results

--- mappings.lua
+++ mappings.lua
@@ -3,7 +3,7 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["£"] = { "#", "hash symbol", opts = { nowait = true } },
     ["0"] = { "^", "go to line start", opts = { nowait = true } },
     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
@@ -11,6 +11,8 @@ M.general = {
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
     ["<C-d>"] = { "<C-d>zz", "Page down and center" },
     ["<C-u>"] = { "<C-u>zz", "Page up and center" },
+    ["N"] = { "Nzz", "Previous found item centered" },
+    ["n"] = { "nzz", "Next found item centered" },
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
     ["<leader><F5>"] = { "<cmd> UndotreeToggle<CR>", "Toggle UndoTree" },
     ["<leader>gg"] = { "<cmd> LazyGit <CR>", "Open LazyGit", opts = { silent = true } },
